7701dacc2c92a7baadb96d02955254c04fd28c7d
dylanclements <dclements@protonmail.com>
2019-05-14 13:51:15 -0400

    Completed all tracks view page with connections to page.

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,7 +1,7 @@
 class TracksController < ApplicationController
     get '/show' do
         erb :'/tracks/show'
-    ends
+    end
 
     get '/tracks' do
         @tracks = Track.all
--- config.ru
+++ config.ru
@@ -6,4 +6,5 @@ end
 
 use UsersController
 use ReviewsController
+use TracksController
 run ApplicationController
