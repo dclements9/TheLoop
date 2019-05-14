5bdda41f3183ad806232c38e472add6b40b7d47e
dylanclements <dclements@protonmail.com>
2019-05-14 12:20:21 -0400

    Added user_id & track_id to reviews and all/user_reviews page.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,7 @@ class ReviewsController < ApplicationController
 
     get '/reviews/:id' do
         @review = Review.find(params[:id])
+        @user = User.find(session[:user_id])
         erb :'reviews/show'
     end
 
@@ -15,12 +16,24 @@ class ReviewsController < ApplicationController
             @track = Track.create(params[:track]) 
         end
         @review.track_name = @track.name
+        @review.user_id=session[:user_id]
+        @review.track_id=@track.id
+        
         @review.save
         redirect "reviews/#{@review.id}"
     end
 
     get '/all' do
         @reviews = Review.all
+        @user = User.find(session[:user_id])
+        
         erb :'/reviews/all'
+    end
+
+    get '/all_user_reviews' do
+        @reviews = Review.all
+        @user = User.find(session[:user_id])
+        
+        erb :'/reviews/user_all'
     end
 end
