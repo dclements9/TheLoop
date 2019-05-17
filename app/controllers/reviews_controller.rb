0282da486e80b8030ecfa2f55082d4bce2841889
dylanclements <dclements@protonmail.com>
2019-05-17 13:51:43 -0400

    Added MethodOverride to config. Added track name validations and output errors to user.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,18 +11,27 @@ class ReviewsController < ApplicationController
     end
 
     post '/new' do
-        @review = Review.new(params[:review])
-        if params[:track_radio] != ""
-            @track = Track.find(params[:track_radio])
-        elsif params[:track][:name] != ""
-            @track = Track.create(params[:track]) 
-        end
-        @review.track_name = @track.name
-        @review.user_id=session[:user_id]
+        @tracks = Track.all
+        if params[:track_radio] == "" && params[:track][:name] == ""
+            @params_check = false
+            erb :'/reviews/new'
+        elsif params[:track_radio] != "" && params[:track][:name] != ""
+            @params_single_checked = false
+            erb :'/reviews/new'
+        else
+            @review = Review.new(params[:review])
+            if params[:track_radio] != ""
+                @track = Track.find(params[:track_radio])
+            elsif params[:track][:name] != ""
+                @track = Track.create(params[:track]) 
+            end
+            @review.track_name = @track.name
+            @review.user_id=session[:user_id]
 
-        @review.track_id=@track.id     
-        @review.save
-        redirect "reviews/#{@review.id}"
+            @review.track_id=@track.id     
+            @review.save
+            redirect "reviews/#{@review.id}"
+        end
     end
 
     get '/all' do
@@ -56,13 +65,26 @@ class ReviewsController < ApplicationController
     end
 
     patch '/reviews/:id' do
+        @tracks = Track.all
         @review = Review.find(params[:id])
-        @review.update(params[:review])
-                if !params[:owner][:name].empty?
-                @pet.owner = Owner.create(name: params[:owner][:name])
-                end
-        @review.save
-        redirect "reviews/#{@review.id}"
+        if params[:track_radio] == "" && params[:track][:name] == ""
+            @params_check = false
+            erb :'/reviews/edit'
+        elsif params[:track_radio] != "" && params[:track][:name] != ""
+            @params_single_checked = false
+            erb :'/reviews/edit'
+        else
+            @review.update(params[:review])
+            if params[:track_radio] !=0 && params[:track][:name]
+                @track = Track.find_by(params[:track_radio])
+                @review.track_name = @track.name
+            elsif params[:track][:name] != ""
+                @track = Track.create(params[:track]) 
+                @review.track_name = params[:track][:name]
+            end
+            @review.save
+            redirect "reviews/#{@review.id}"
+        end
     end
 
     get '/reviews/:id/delete' do
